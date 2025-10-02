f6997597bf47dd66fdf803442434f0ad9e7f3e19
shanAweb <shanAweb>
2025-10-02 09:01:47 +0500

    Update: Created and added as well as configured static files

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("<h3>This is our project</h3>")
+    return render(request, 'home.html')
